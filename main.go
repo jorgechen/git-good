62f1bf360aa91ce591395c0e76a7a9d87baaf592
George C <george.chen@arundo.com>
2018-07-23 11:05:23 -0500

    The coffee instructions header

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("How to Make Coffee: (Note: only includes first 3 steps!")
+	fmt.Println("Coffee Making:")
 	fmt.Println("1. Clean out pot")
 	fmt.Println("2. Take top cup")
 	fmt.Println("3. Dump grinds")
