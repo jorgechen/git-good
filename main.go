3f985c17333bb6e89a44a3c8ed0da356b235fc6d
George Chen <jorgechen@users.noreply.github.com>
2018-07-23 14:28:16 +0000

    AB-200: Making Coffee, part 1 (#7)
    * first step to make coffee
    
    * step 2
    
    * step 3
    
    * updated first printed line
    
    * space

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("How to Make Coffee: (Note: only includes first 3 steps!")
+	fmt.Println("1. Clean out pot")
+	fmt.Println("2. Take top cup")
+	fmt.Println("3. Dump grinds")
 }
