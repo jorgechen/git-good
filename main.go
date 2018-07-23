61cdd6ca3f517fb007c7f582e6244176b60bda77
George C <george.chen@arundo.com>
2018-07-23 09:24:45 -0500

    space

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("How to Make Coffee: (Note: only includes first 3 steps!")
-	fmt.Println("1.Clean out pot")
+	fmt.Println("1. Clean out pot")
 	fmt.Println("2. Take top cup")
 	fmt.Println("3. Dump grinds")
 }
